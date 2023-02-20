e557d5c7772bd7cbaca6e4f7fb465e7f0d417e4f
Radosław <teammenpl@gmail.com>
2023-02-20 22:14:41 +0100

    Web app startup

---
+++ SchoolGradeManager/Controllers/StudentController.cs
@@ -0,0 +1,91 @@
+﻿using MessagePack.Resolvers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolGradeManager.Models;
+
+namespace SchoolGradeManager.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static IList<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                StudentFirstName = "Radosław",
+                StudentLastName = "Barczyński",
+                StudentEmail = "radbar@utp.edu.pl"
+            }
+        };
+
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View(new Student());
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            student.Id = students.Count + 1;
+            students.Add(student);            
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SchoolGradeManager/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolGradeManager.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public string StudentEmail { get; set; }
+    }
+}
