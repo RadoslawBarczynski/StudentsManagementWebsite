2a8829b1cae6b3a289bde87d1dfd8ccef4df3f11
Radosław <teammenpl@gmail.com>
2023-05-24 23:39:00 +0200

    SHA256 Hash

--- SchoolGradeManager/Repositories/StudentRepository.cs
+++ SchoolGradeManager/Repositories/StudentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SchoolGradeManager.Models;
-using BCrypt.Net;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SchoolGradeManager.Repositories
 {
@@ -21,12 +22,23 @@ namespace SchoolGradeManager.Repositories
 
         public IQueryable<Student> GetAllActive() => _context.students.Include("grade");
 
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hashedPassword = BitConverter.ToString(hashedBytes).Replace("-", string.Empty);
+                return hashedPassword;
+            }
+        }
+
 
         public void Add(Student student)
         {
             Guid myuuid = Guid.NewGuid();
             student.id = myuuid;
-            student.StudentPassword = BCrypt.Net.BCrypt.HashPassword(student.StudentPassword, BCrypt.Net.BCrypt.GenerateSalt(12));
+            //student.StudentPassword = BCrypt.Net.BCrypt.HashPassword(student.StudentPassword, BCrypt.Net.BCrypt.GenerateSalt(12));
+            student.StudentPassword = HashPassword(student.StudentPassword).ToLower();
             _context.students.Add(student);
             //basic grade template
             Grade grade = new Grade();
@@ -63,7 +75,7 @@ namespace SchoolGradeManager.Repositories
                 result.StudentFirstName = student.StudentFirstName;
                 result.StudentLastName = student.StudentLastName;
                 result.StudentLogin = student.StudentLogin;
-                result.StudentPassword = student.StudentPassword;
+                result.StudentPassword = HashPassword(student.StudentPassword).ToLower();
 
                 _context.SaveChanges();
             }
---
+++ SchoolGradeManager/Services/PasswordHashing.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System;
+using System.Security.Cryptography;
+
+namespace SchoolGradeManager.Services
+{
+    public class PasswordHashing
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hashedPassword = BitConverter.ToString(hashedBytes).Replace("-", string.Empty);
+                return hashedPassword;
+            }
+        }
+
+        public static void Main()
+        {
+            string password = "password";
+            string hashedPassword = HashPassword(password);
+
+            Console.WriteLine("Zahaszowane hasło: " + hashedPassword);
+        }
+    }
+}
